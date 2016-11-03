bd0fcb462530016016074fbf1553e1d681c3c496
jelle van loock <r0484335@student.thomasmore.be>
2016-11-03 12:43:41 +0100

    1. Inladen express, bodyparser. Poort 4567

---
+++ main.js
@@ -0,0 +1,16 @@
+// inladen van de dependencies - externe dependencies inladen via het commando: 
+// npm install express --save
+// npm install body-parser --save
+
+var express = require('express'); // eenvoudige webserver in node js
+var parser = require('body-parser'); // extensie op express voor eenvoudig body uit te lezen
+
+// aanmaken van de webserver variabele
+var app = express();
+// automatische json-body parsers van request MET media-type application/json gespecifieerd in de request.
+app.use(parser.json());
+
+// de server starten op poort 4567 (bereikbaar op http://localhost:4567 )
+app.listen(4567);
+// lijntje voor te zien dat alles is opgestart.
+console.log("Server started");
