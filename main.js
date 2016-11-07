633c4416a6630a50b60af584b86befb86671b554
jelle van loock <r0484335@student.thomasmore.be>
2016-11-07 14:30:35 +0100

    9. Enkele aanpassingen (samengewerkt met Galvani, Jonnick, Genzo en Kevin)

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-// inladen van de dependencies - externe dependencies inladen via het commando: 
-// npm install express --save
-// npm install body-parser --save
+//Samen gewerkt met Jelle Van Loock en Kevin Aerts
 
+// inladen van de dependencies
 var express = require('express'); // eenvoudige webserver in node js
 var parser = require('body-parser'); // extensie op express voor eenvoudig body uit te lezen
 var uuid = require('uuid');
 
 // Toevoegen van de code van de dal vervangt onze
-// onze lokale 'datastore'. deze variable bewaart onze state. 
-var dal = require("./storageLocaties.js");
-var dal2 = require("./storageAanwezigheden.js");
+// onze lokale 'datastore'. deze variable bewaart onze state.
+var dal = require("./storage.js");
 //validatie inladen
 var validation = require("./validate.js");
 
@@ -18,89 +16,97 @@ var app = express();
 // automatische json-body parsers van request MET media-type application/json gespecifieerd in de request.
 app.use(parser.json());
 
-// opvangen van een GET op /locaties
-app.get("/locaties", function (request, response) {
-  //stuurt als antwoord de inhoud van onze database. Standaard in json terug gestuurd.
-  response.send(dal.AllLocaties());
+// locaties beschikbaar maken
+app.get("/locaties", function(request, response) {
+    //Antwoord met alle instanties van /locaties in json-formaat
+    response.send(dal.AllLocaties());
 });
 
-// opvangen van een GET op /locaties/{locatie_naam}
-app.get("/locaties/:id", function (request, response) {
-  var locatie = dal.findLocatie(request.params.id);
-  if(locatie) {
-    response.send(locatie);
-  }else {
-    response.status(404).send();
-  }
+// opvangen van een GET op /locaties/[locatie_naam]
+app.get("/locaties/:id", function(request, response) {
+    var locatie = dal.findLocatie(request.params.id);
+    if (locatie) {
+        response.send(locatie);
+    } else {
+        response.status(404).send();
+    }
 });
 
-// opvangen van een POST op /locaties. 
-app.post("/locaties", function(request, response){
-  // de data in de body wordt toegekend aan onze locatie variabele. 
-  // deze is enkel opgevuld indien het JSON is.
-  var locatie = request.body;
-  
+// opvangen van POST's op /locaties.
+app.post("/locaties", function(request, response) {
+    // de data in de body wordt toegekend aan onze locatie variabele.
+    // deze is enkel opgevuld indien het JSON is.
+    var locatie = request.body;
+
     // Valideren dat velden bestaan
-  var errors = validation.fieldsNotEmpty(locatie, "naam", "mac_address_drone", "beschrijving");
-  if (errors){
-    response.status(400).send({msg:"Following field(s) are mandatory:"+errors.concat()});
-    return;
-  }
-  
- // Valideren dat we niet dezelfde locatie 2x hebben
-  var existingLocatie = dal.findLocatie(locatie.naam);
-  if(existingLocatie){
-    response.status(409).send({msg:"Locatienaam must be unique, it's already registered", link:"../locaties/"+existingLocatie.id});
-    return;
-  }
-  
-  // een id geven aan ons nieuwe locatie.
-  locatie.id = locatie.naam;
-  // de locatie toevoege in onze 'dal'.
-  dal.saveLocatie(locatie);
-// de default httpstatus (200) overschrijven met 204 en geen antwoord specifiëren.
-  response.status(201).location("../locaties/"+locatie.id).send();
+    var errors = validation.fieldsNotEmpty(locatie, "naam", "mac_address_drone", "beschrijving");
+    if (errors) {
+        response.status(400).send({
+            msg: "Following field(s) are mandatory:" + errors.concat()
+        });
+        return;
+    }
+
+    // Valideren dat we niet dezelfde locatie 2x hebben
+    var existingLocatie = dal.findLocatie(locatie.naam);
+    if (existingLocatie) {
+        response.status(409).send({
+            msg: "Locatienaam must be unique, it's already registered",
+            link: "../locaties/" + existingLocatie.id
+        });
+        return;
+    }
+
+    // De naam van de locatie wordt toegekend als ID
+    locatie.id = locatie.naam;
+    // de locatie toevoegen in onze de lokale opslag 'dal'.
+    dal.saveLocatie(locatie);
+    // de default httpstatus (200) overschrijven met 204 en geen antwoord specifiÃ«ren.
+    response.status(201).location("../locaties/" + locatie.id).send();
 });
 
 
 // opvangen van een GET op /aanwezigheden
-app.get("/aanwezigheden", function (request, response) {
-  //stuurt als antwoord de inhoud van onze database. Standaard in json terug gestuurd.
-  response.send(dal2.AllAanwezigheden());
+app.get("/aanwezigheden", function(request, response) {
+    //stuurt als antwoord de inhoud van onze database. Standaard in json terug gestuurd.
+    response.send(dal.AllAanwezigheden());
 });
 
-// opvangen van een GET op /aanwezigheden/{locatie_naam}
-app.get("/aanwezigheden/:id", function (request, response) {
-  var locatie = dal2.findAanwezighedenPerLocatie(request.params.id);
-  if(locatie) {
-    response.send(locatie);
-  }else {
-    response.status(404).send();
-  }
+// opvangen van een GET op /aanwezigheden/[UUID]
+app.get("/aanwezigheden/:id", function(request, response) {
+    var locatie = dal.findAanwezighedenPerLocatie(request.params.id);
+    if (locatie) {
+        response.send(locatie);
+    } else {
+        response.status(404).send();
+    }
 });
 
-// opvangen van een POST op /locaties. 
-app.post("/aanwezigheden", function(request, response){
-  // de data in de body wordt toegekend aan onze locatie variabele. 
-  // deze is enkel opgevuld indien het JSON is.
-  var personen = request.body;
-  
+// POST's op /aanwezigheden opvangen
+app.post("/aanwezigheden", function(request, response) {
+    // de data in de body wordt toegekend aan onze locatie variabele.
+    // deze is enkel opgevuld indien het JSON is.
+    var personen = request.body;
+
     // Valideren dat velden bestaan
-  var errors = validation.fieldsNotEmpty(personen, "drone_naam","aantal", "locatie_naam", "uur");
-  if (errors){
-    response.status(400).send({msg:"Following field(s) are mandatory:"+errors.concat()});
-    return;
-  }
-  
-  // een id geven aan ons nieuwe locatie.
-  personen.id = uuid.v4();
-  // de locatie toevoege in onze 'dal'.
-  dal2.saveAanwezighedenPerLocatie(personen);
-// de default httpstatus (200) overschrijven met 204 en geen antwoord specifiëren.
-  response.status(201).location("../aanwezigheden/"+personen.id).send();
+    var errors = validation.fieldsNotEmpty(personen, "drone_naam", "aantal", "locatie_naam", "uur");
+    if (errors) {
+        response.status(400).send({
+            msg: "Following field(s) are mandatory:" + errors.concat()
+        });
+        return;
+    }
+
+    // een random UUID geven aan ons nieuwe 'persoon'.
+    personen.id = uuid.v4();
+    // de 'persoon' toevoegen in onze 'dal'.
+    dal.saveAanwezighedenPerLocatie(personen);
+    // de default httpstatus (200) overschrijven met 204 en geen antwoord specifiÃ«ren.
+    response.status(201).location("../aanwezigheden/" + personen.id).send();
 });
 
-// de server starten op poort 4567 (bereikbaar op http://localhost:4567 )
+// de server starten op poort 4567
 app.listen(4567);
-// lijntje voor te zien dat alles is opgestart.
+
+// Bevestiging
 console.log("Server started");
--- storage.js
+++ storage.js
@@ -1,17 +1,33 @@
-module.exports =  {
-  locaties : {},
-  
-  saveLocatie: function(locatie){
-    this.locaties[locatie.id]= locatie;
-  },
-  AllLocaties : function(){
-    var rtnValue =[];
-    for (var item in this.locaties) {
-      rtnValue.push(this.locaties[item]);
-    };
-    return rtnValue;
-  },
-  findLocatie : function(id){
-    return this.locaties[id];
-  }
+module.exports = {
+    aanwezigheden: {},
+
+    saveAanwezighedenPerLocatie: function(locatie) {
+        this.aanwezigheden[locatie.id] = locatie;
+    },
+    AllAanwezigheden: function() {
+        var rtnValue = [];
+        for (var item in this.aanwezigheden) {
+            rtnValue.push(this.aanwezigheden[item]);
+        };
+        return rtnValue;
+    },
+
+    findAanwezighedenPerLocatie: function(naam) {
+        return this.aanwezigheden[naam];
+    },
+    locaties: {},
+
+    saveLocatie: function(locatie) {
+        this.locaties[locatie.id] = locatie;
+    },
+    AllLocaties: function() {
+        var rtnValue = [];
+        for (var item in this.locaties) {
+            rtnValue.push(this.locaties[item]);
+        };
+        return rtnValue;
+    },
+    findLocatie: function(id) {
+        return this.locaties[id];
+    }
 };
